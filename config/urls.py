b7d2ad31df0bae1e9907792d78a57948376e9e0e
Andreas Nüßlein <andreas@nuessle.in>
2016-02-24 18:47:37 +0100

    init

---
+++ config/urls.py
@@ -0,0 +1,47 @@
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.views import defaults as default_views
+from django.views.generic import TemplateView
+from wagtail.wagtailadmin import urls as wagtailadmin_urls
+from wagtail.wagtailcore import urls as wagtail_urls
+from wagtail.wagtaildocs import urls as wagtaildocs_urls
+
+urlpatterns = [
+    # url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name="home"),
+    # url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name="about"),
+
+    # Django Admin, use {% url 'admin:index' %}
+    url(settings.ADMIN_URL, include(admin.site.urls)),
+
+    url(r'^admin/', include(wagtailadmin_urls)),
+    url(r'^documents/', include(wagtaildocs_urls)),
+
+    url(r'', include(wagtail_urls)),
+
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    from django.views.generic import TemplateView
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    # This allows the error pages to be debugged during development, just visit
+    # these url in browser to see how these error pages look like.
+    urlpatterns += [
+        url(r'^400/$', default_views.bad_request, kwargs={'exception': Exception("Bad Request!")}),
+        url(r'^403/$', default_views.permission_denied, kwargs={'exception': Exception("Permission Denied")}),
+        url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception("Page not Found")}),
+        url(r'^500/$', default_views.server_error),
+    ]
---
+++ fabfile.py
@@ -0,0 +1,70 @@
+from fabric.api import cd, run, env
+from fabvenv import virtualenv
+
+
+def staging():
+    projectname = 'isi-mip'
+
+    basepath = '/srv/isi-mip.sinnwerkstatt.com/%s'
+
+    env.hosts = ['{0}@{0}.sinnwerkstatt.com'.format(projectname)]
+    env.path = basepath % projectname
+    env.virtualenv_path = basepath % (projectname+'env')
+    env.backup_path = basepath % 'backups'
+    env.push_branch = 'staging'
+    env.push_remote = 'origin'
+    env.reload_cmd = 'supervisorctl restart {0}'.format(projectname)
+    env.db_name = projectname
+    env.db_username = projectname
+    env.after_deploy_url = 'http://%s.sinnwerkstatt.com' % projectname
+
+
+def production():
+    pass
+
+
+def reload_webserver():
+    run("%(reload_cmd)s" % env)
+
+
+def migrate():
+    with virtualenv(env.virtualenv_path):
+        run("%(path)s/manage.py syncdb" % env)
+        run("%(path)s/manage.py migrate" % env)
+
+
+def ping():
+    run(
+        "echo %(after_deploy_url)s returned:  \>\>\>  $(curl --write-out %%{http_code} --silent --output /dev/null %(after_deploy_url)s)" % env)
+
+
+def deploy():
+    with cd(env.path):
+        run("git pull %(push_remote)s %(push_branch)s" % env)
+        with virtualenv(env.virtualenv_path):
+            run("pip install -Ur requirements.txt")
+            run("./manage.py collectstatic --noinput")
+            # run("cd yooweedoo_contest && ../manage.py compilemessages")
+
+    migrate()
+    reload_webserver()
+    ping()
+
+#
+# def init_fixtures():
+#     with virtualenv(env.virtualenv_path):
+#         run("%(path)s/manage.py loaddata init.json" % env)
+#
+#
+# def update():
+#     ''' Only deploy and reload modules from git, do no installing or migrating'''
+#     with cd(env.path):
+#         run("git pull %(push_remote)s %(push_branch)s" % env)
+#
+#     reload_webserver()
+#
+#
+# def backup():
+#     with cd(env.backup_path):
+#         run("pg_dump -U %(db_username)s %(db_name)s > %(db_name)s_backup_$(date +%%F-%%T).sql" % env)
+#         run("ls -lt")
