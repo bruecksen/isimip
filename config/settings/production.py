4dc5d00703a17c9962d3be275e2da4b598a0f110
Andreas Nüßlein <andreas@nuessle.in>
2016-03-01 16:00:55 +0100

    step

--- config/settings/production.py
+++ config/settings/production.py
@@ -12,8 +12,8 @@ Production Configurations
 '''
 from __future__ import absolute_import, unicode_literals
 
-from boto.s3.connection import OrdinaryCallingFormat
-from django.utils import six
+# from boto.s3.connection import OrdinaryCallingFormat
+# from django.utils import six
 
 import logging
 
