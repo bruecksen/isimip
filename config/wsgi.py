ece95793ce4ca09ef2ea8946b2ed4fafba0578cd
Andreas Nüßlein <andreas@nuessle.in>
2016-03-01 16:29:58 +0100

    step

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,6 +1,12 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
+import environ
+
+ROOT_DIR = environ.Path(__file__) -2
+env = environ.Env()
+env.read_env(ROOT_DIR('.env'))
+
 if os.environ.get("DJANGO_SETTINGS_MODULE") == "config.settings.production":
     from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
