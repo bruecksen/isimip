b33a68bb1ce5f29e5b30b11e02c543b6aa8c01ec
Andreas Nüßlein <andreas@nuessle.in>
2016-05-04 13:10:31 +0200

    improved wagtail menus

--- isi_mip/pages/wagtail_hooks.py
+++ isi_mip/pages/wagtail_hooks.py
@@ -2,6 +2,9 @@ from django.conf import settings
 from django.utils.html import format_html_join
 from wagtail.wagtailcore import hooks
 
+from isi_mip.climatemodels.models import ImpactModel, InputData, OutputData
+from django.utils.safestring import mark_safe
+
 
 @hooks.register('insert_editor_css')
 def editor_css():
@@ -27,6 +30,7 @@ class DjangoAdminLinkItem:
                     </div>
                 </div>'''
 
+
 class LogOutLinkItem:
     def render(self, request):
         return '''<div class="wagtail-userbar__item ">
@@ -35,7 +39,57 @@ class LogOutLinkItem:
             </div>
         </div>'''
 
+
 @hooks.register('construct_wagtail_userbar')
 def add_puppy_link_item(request, items):
     items.append(DjangoAdminLinkItem())
     items.append(LogOutLinkItem())
+
+
+class ImpactModelsPanel(object):
+    order = 50
+
+    def render(self):
+        impactmodels = ImpactModel.objects
+        inputdata = InputData.objects
+        outputdata = OutputData.objects
+        output = """<section class="panel summary nice-padding">
+                    <h2 class="visuallyhidden">Impact Model summary</h2>
+                    <ul class="stats">
+                            <li class="icon icon-cogs">
+                        <a href="/admin/climatemodels/impactmodel/">
+                            <span>{}</span> Impact Models
+                        </a>
+                    </li>
+                    <li class="icon icon-order-down">
+                        <a href="/admin/climatemodels/inputdata/">
+                            <span>{}</span> Input Data sets
+                        </a>
+                    </li>
+                    <li class="icon icon-order-up">
+                        <a href="/admin/climatemodels/outputdata/">
+                            <span>{}</span> Output Data sets
+                        </a>
+                    </li>
+                    </ul>
+                    </section>
+                    """.format(impactmodels.count(), inputdata.count(), outputdata.count())
+
+        return mark_safe(output)
+
+
+@hooks.register('construct_homepage_panels')
+def add_another_welcome_panel(request, panels):
+    return panels.append(ImpactModelsPanel())
+
+class Dings:
+    order = 90000
+    def render_html(self, request):
+        output = '''<li class="menu-item">
+                        <a href="/admin/" class="icon icon-pick">Django Admin</a>
+                    </li>'''
+        return output
+
+@hooks.register('construct_main_menu')
+def hide_explorer_menu_item_from_frank(request, menu_items):
+    menu_items.append(Dings())
