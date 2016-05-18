2cb9c556f05ed9ab385c8752c06cc28b52182aac
Thomas Schmidt <ts@sinnwerkstatt.com>
2016-05-18 15:38:58 +0200

    Fix Styling #88 und #81

--- isi_mip/static/js/isi_mip.js
+++ isi_mip/static/js/isi_mip.js
@@ -330,3 +330,15 @@ $(function() {
 	});
 });
 
+
+// Smooth scrolling to anchors
+// https://stackoverflow.com/questions/14804941
+$("a[href^='#']").on('click', function(e) {
+	e.preventDefault();
+	var hash = this.hash;
+	$('html, body').animate({
+		scrollTop: $(this.hash).offset().top
+	}, 300, function(){
+		window.location.hash = hash;
+	});
+});
