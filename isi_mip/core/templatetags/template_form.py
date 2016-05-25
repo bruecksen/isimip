0a522ae1c448d8e709c7602c3689086af50b8df6
Andreas Nüßlein <andreas@nuessle.in>
2016-05-25 17:24:14 +0200

    diverses

--- isi_mip/core/templatetags/template_form.py
+++ isi_mip/core/templatetags/template_form.py
@@ -4,6 +4,8 @@ from django.forms import BaseForm
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe, mark_for_escaping
 
+from isi_mip.climatemodels.widgets import MyTextInput, MyMultiSelect, MyBooleanSelect
+
 register = template.Library()
 
 
@@ -34,7 +36,45 @@ def template_form(form, **kwargs):
     assert isinstance(form, BaseForm)
     newform = SimpleStringForm()
     for field in form:
-        template = 'widgets/formfeld.html'
-        string = render_to_string(template, {'field': field})
+        context = {'error': field.errors,
+                   'help': field.help_text,
+                   'id': field.name,
+                   'label': field.label,
+                   # 'type': field.field.widget.input_type,
+                   #'type': 'number', # email, password
+                   'value': field.value,
+                   'required': field.field.required,
+                   }
+        context['readonly'] = 'readonly' in field.field.widget.attrs and field.field.widget.attrs['readonly']
+
+
+        if isinstance(field.field.widget, MyTextInput):
+            template = 'widgets/textinput.html'
+        elif isinstance(field.field.widget, MyBooleanSelect):
+            context['nullable'] = field.field.widget.nullable
+            template = 'widgets/nullboolean.html'
+        elif isinstance(field.field.widget, MyMultiSelect):
+            context.update({
+                'allowcustom': field.field.widget.allowcustom,
+                'singleselect': not field.field.widget.multiselect,
+                'options': []
+            })
+            value = field.value()
+            for k, v in field.field.choices:
+                if isinstance(value, list):
+                    checked = str(k) in [str(x) for x in value]
+                else:
+                    checked = k == value
+                if v == '---------' and not k:
+                    continue
+                context['options'] += [{'checked': checked, 'label': v, 'value': k}]
+
+            if not any(opt['checked'] for opt in context['options']) and value:
+                context['options'] += [{'checked': True, 'label': value, 'value': value}]
+
+            template = 'widgets/multiselect.html'
+        else:
+            template = 'widgets/textinput.html'
+        string = render_to_string(template, context)
         newform[field.name] = string
     return newform
