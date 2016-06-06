53467b2218261e1137093f0f496d946a617e53f2
Thomas Schmidt <ts@sinnwerkstatt.com>
2016-06-06 12:41:17 +0200

    Icon Context #136

--- isi_mip/styleguide/data.py
+++ isi_mip/styleguide/data.py
@@ -372,6 +372,7 @@ data = {
             },
             'title': 'Impact Models',
             'date': '8/12/2016',
+            'calendaricon': True,
             'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui.',
             'text_right_link': True,
             'text_right_link_text': 'Learn more',
