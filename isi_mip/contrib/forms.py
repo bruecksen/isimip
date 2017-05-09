8000c013d4fb64258047a125c531b0376f92d1bb
Matthias Brück <hi@brueck.io>
2017-05-09 15:51:40 +0200

    add proper lost password views

--- isi_mip/contrib/forms.py
+++ isi_mip/contrib/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import (
-    AuthenticationForm, UsernameField )
+    AuthenticationForm, UsernameField)
 
 
 class AuthenticationForm(AuthenticationForm):
