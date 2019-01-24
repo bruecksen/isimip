54dae01585a45d2ee652bd849078a11d64a9aea3
Matthias Brück <hi@brueck.io>
2019-01-24 20:35:08 +1000

    fix copy paste error

--- isi_mip/climatemodels/admin.py
+++ isi_mip/climatemodels/admin.py
@@ -312,7 +312,7 @@ class DataPublicationConfirmationModelAdmin(admin.ModelAdmin):
                 'model_contact_person': owner.name,
                 'simulation_round': confirmation.impact_model.simulation_round,
                 'sector': confirmation.impact_model.base_model.sector,
-                'sector_drkz_folder_name': confirmation.impact_model.base_model.drkz_folder_name,
+                'sector_drkz_folder_name': confirmation.impact_model.base_model.sector.drkz_folder_name,
                 'impact_model_drkz_folder_name': confirmation.impact_model.base_model.drkz_folder_name,
                 'impact_model_name': confirmation.impact_model.base_model.name,
                 'data_confirmation_link': link,
