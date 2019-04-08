035da688bfc6ff43c9a4e86b9bb6aabaf006d139
Matthias Brück <hi@brueck.io>
2019-04-08 12:43:57 +0200

    fix migrations

--- isi_mip/climatemodels/migrations/0098_auto_20190119_0603.py
+++ isi_mip/climatemodels/migrations/0098_auto_20190119_0603.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('climatemodels', '0090_auto_20190119_0555'),
+        ('climatemodels', '0097_auto_20190119_0555'),
     ]
 
     operations = [
