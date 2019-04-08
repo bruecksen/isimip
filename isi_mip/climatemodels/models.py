bb1b19bf47e21c78773a495592ca00a3de63abb5
Matthias Brück <hi@brueck.io>
2019-04-08 11:14:49 +0200

    change helpt text for clarification

--- isi_mip/climatemodels/models.py
+++ isi_mip/climatemodels/models.py
@@ -255,7 +255,7 @@ class BaseImpactModel(index.Indexed, models.Model):
     region = models.ManyToManyField(Region, help_text="Region for which model produces results")
     short_description = models.TextField(
         null=True, blank=True, default='', verbose_name="Short model description",
-        help_text="This short description should assist other researchers in getting an understanding of your model, including the main differences between model versions used for different ISIMIP simulation rounds.")
+        help_text="This information applies to all modeling rounds. Please do not erase this information, just add when necessary. This short description should assist other researchers in getting an understanding of your model, including the main differences between model versions used for different ISIMIP simulation rounds.")
 
     search_fields = [
         index.SearchField('name', partial_match=True, boost=10),
